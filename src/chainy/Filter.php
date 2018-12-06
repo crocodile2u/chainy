176f3297079bcf4e9bb2b5bc5b49f44321c320cc
Benjamin Yazi <benjamin.yazi@gmail.com>
2018-12-06 22:19:30 +0200

    Added support filter flags, resolved #3 issue

--- src/chainy/Filter.php
+++ src/chainy/Filter.php
@@ -8,18 +8,24 @@ class Filter
      * @var callable|null
      */
     private $callback;
+    /**
+     * @var int|null
+     */
+    private $flag;
 
     /**
      * Filter constructor.
      * @param callable $callback
+     * @param int $flag
      */
-    public function __construct($callback = null)
+    public function __construct($callback = null, $flag = 0)
     {
         $this->callback = $callback;
+        $this->flag = $flag;
     }
 
     public function __invoke(array $value)
     {
-        return $this->callback ? array_filter($value, $this->callback) : array_filter($value);
+        return array_filter($value, $this->callback, $this->flag);
     }
 }
