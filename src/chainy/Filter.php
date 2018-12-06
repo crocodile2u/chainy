31f9015c832b78e6e773c1abdf08e4f1dfee83d1
Benjamin Yazi <benjamin.yazi@gmail.com>
2018-12-06 22:35:54 +0200

    added default value to filter function resolved #3 issue

--- src/chainy/Filter.php
+++ src/chainy/Filter.php
@@ -4,21 +4,22 @@ namespace chainy;
 
 class Filter
 {
+    const FLAG_DEFAULT = 0;
     /**
      * @var callable|null
      */
     private $callback;
     /**
-     * @var int|null
+     * @var integer|null
      */
     private $flag;
 
     /**
      * Filter constructor.
      * @param callable $callback
-     * @param int $flag
+     * @param integer $flag
      */
-    public function __construct($callback = null, $flag = 0)
+    public function __construct($callback = null, $flag = self::FLAG_DEFAULT)
     {
         $this->callback = $callback;
         $this->flag = $flag;
@@ -26,6 +27,6 @@ class Filter
 
     public function __invoke(array $value)
     {
-        return array_filter($value, $this->callback, $this->flag);
+        return $this->callback ? array_filter($value, $this->callback, $this->flag) : array_filter($value);
     }
 }
